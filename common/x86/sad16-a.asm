a30b160383105e120b17a2d12f6055041c02d29c
Alexander Gattin <xrgtn@yandex.ru>
2024-07-24 02:51:53 +0000

    add i386 PIC support to sad16-a.asm

--- common/x86/sad16-a.asm
+++ common/x86/sad16-a.asm
@@ -37,7 +37,7 @@ cextern pw_8
 ; SAD MMX
 ;=============================================================================
 
-%macro SAD_INC_1x16P_MMX 0
+%macro SAD_INC_1x16P_MMX 0 ; r0..r3
     movu    m1, [r0+ 0]
     movu    m2, [r0+ 8]
     movu    m3, [r0+16]
@@ -56,7 +56,7 @@ cextern pw_8
     paddw   m0, m3
 %endmacro
 
-%macro SAD_INC_2x8P_MMX 0
+%macro SAD_INC_2x8P_MMX 0 ; r0..r3
     movu    m1, [r0+0]
     movu    m2, [r0+8]
     movu    m3, [r0+2*r1+0]
@@ -75,7 +75,7 @@ cextern pw_8
     paddw   m0, m3
 %endmacro
 
-%macro SAD_INC_2x4P_MMX 0
+%macro SAD_INC_2x4P_MMX 0 ; r0..r3
     movu    m1, [r0]
     movu    m2, [r0+2*r1]
     psubw   m1, [r2]
@@ -94,7 +94,7 @@ cextern pw_8
 cglobal pixel_sad_%1x%2, 4,5-(%2&4/4)
     pxor    m0, m0
 %if %2 == 4
-    SAD_INC_%3x%1P_MMX
+    SAD_INC_%3x%1P_MMX ; r0..r3
     SAD_INC_%3x%1P_MMX
 %else
     mov    r4d, %2/%3
@@ -106,7 +106,7 @@ cglobal pixel_sad_%1x%2, 4,5-(%2&4/4)
 %if %1*%2 == 256
     HADDUW  m0, m1
 %else
-    HADDW   m0, m1
+    HADDW   m0, m1 ; PIC*
 %endif
     movd   eax, m0
     RET
@@ -128,7 +128,7 @@ SAD_MMX  4,  4, 2
 ; SAD XMM
 ;=============================================================================
 
-%macro SAD_INC_2ROW 1
+%macro SAD_INC_2ROW 1 ; r0..r3
 %if 2*%1 > mmsize
     movu    m1, [r2+ 0]
     movu    m2, [r2+16]
@@ -166,7 +166,7 @@ SAD_MMX  4,  4, 2
 cglobal pixel_sad_%1x%2, 4,5-(%2&4/4),8*(%1/mmsize)
     pxor    m0, m0
 %if %2 == 4
-    SAD_INC_2ROW %1
+    SAD_INC_2ROW %1 ; r0..r3
     SAD_INC_2ROW %1
 %else
     mov    r4d, %2/2
@@ -175,7 +175,7 @@ cglobal pixel_sad_%1x%2, 4,5-(%2&4/4),8*(%1/mmsize)
     dec    r4d
     jg .loop
 %endif
-    HADDW   m0, m1
+    HADDW   m0, m1 ; PIC*
     movd   eax, xm0
     RET
 %endmacro
@@ -210,14 +210,14 @@ SAD 16,  8
 ; SAD x3/x4
 ;=============================================================================
 
-%macro SAD_X3_INC_P 0
+%macro SAD_X3_INC_P 0 ; r0..r4
     add     r0, 4*FENC_STRIDE
     lea     r1, [r1+4*r4]
     lea     r2, [r2+4*r4]
     lea     r3, [r3+4*r4]
 %endmacro
 
-%macro SAD_X3_ONE_START 0
+%macro SAD_X3_ONE_START 0 ; r0..r3
     mova    m3, [r0]
     movu    m0, [r1]
     movu    m1, [r2]
@@ -229,7 +229,7 @@ SAD 16,  8
     ABSW    m2, m2, m6
 %endmacro
 
-%macro SAD_X3_ONE 2
+%macro SAD_X3_ONE 2 ; r0..r3
     mova    m6, [r0+%1]
     movu    m3, [r1+%2]
     movu    m4, [r2+%2]
@@ -244,15 +244,23 @@ SAD 16,  8
     paddw   m2, m5
 %endmacro
 
-%macro SAD_X3_END 2
+%macro SAD_X3_END 2 ; r0*, r5*, r5mp*, PIC*, RET
 %if mmsize == 8 && %1*%2 == 256
     HADDUW   m0, m3
     HADDUW   m1, m4
     HADDUW   m2, m5
 %else
-    HADDW    m0, m3
-    HADDW    m1, m4
-    HADDW    m2, m5
+    %if cpuflag(xop) && sizeof%[m0] == 16
+        HADDW    m0, m3
+        HADDW    m1, m4
+        HADDW    m2, m5
+    %else
+        PIC_BEGIN r0, 0
+        HADDW    m0, m3 ; PIC
+        HADDW    m1, m4
+        HADDW    m2, m5
+        PIC_END
+    %endif
 %endif
 %if UNIX64
     movd [r5+0], xm0
@@ -267,7 +275,7 @@ SAD 16,  8
     RET
 %endmacro
 
-%macro SAD_X4_INC_P 0
+%macro SAD_X4_INC_P 0 ; r0..r5
     add     r0, 4*FENC_STRIDE
     lea     r1, [r1+4*r5]
     lea     r2, [r2+4*r5]
@@ -275,7 +283,7 @@ SAD 16,  8
     lea     r4, [r4+4*r5]
 %endmacro
 
-%macro SAD_X4_ONE_START 0
+%macro SAD_X4_ONE_START 0 ; r0..r4
     mova    m4, [r0]
     movu    m0, [r1]
     movu    m1, [r2]
@@ -289,7 +297,7 @@ SAD 16,  8
     ABSW2   m2, m3, m2, m3, m4, m7
 %endmacro
 
-%macro SAD_X4_ONE 2
+%macro SAD_X4_ONE 2 ; r0..r4
     mova    m4, [r0+%1]
     movu    m5, [r1+%2]
     movu    m6, [r2+%2]
@@ -338,17 +346,26 @@ SAD 16,  8
 %endif
 %endmacro
 
-%macro SAD_X4_END 2
+%macro SAD_X4_END 2 ; r0, r6mp, PIC*, RET
 %if mmsize == 8 && %1*%2 == 256
     HADDUW    m0, m4
     HADDUW    m1, m5
     HADDUW    m2, m6
     HADDUW    m3, m7
 %else
-    HADDW     m0, m4
-    HADDW     m1, m5
-    HADDW     m2, m6
-    HADDW     m3, m7
+    %if cpuflag(xop) && sizeof%[m0] == 16
+        HADDW     m0, m4
+        HADDW     m1, m5
+        HADDW     m2, m6
+        HADDW     m3, m7
+    %else
+        PIC_BEGIN r0, 0
+        HADDW     m0, m4
+        HADDW     m1, m5
+        HADDW     m2, m6
+        HADDW     m3, m7
+        PIC_END
+    %endif
 %endif
     mov       r0, r6mp
     movd [r0+ 0], xm0
@@ -358,10 +375,10 @@ SAD 16,  8
     RET
 %endmacro
 
-%macro SAD_X_2xNP 4
+%macro SAD_X_2xNP 4 ; r0..r4*
     %assign x %3
 %rep %4
-    SAD_X%1_ONE x*mmsize, x*mmsize
+    SAD_X%1_ONE x*mmsize, x*mmsize ; X3: r0..r3 / X4: r0..r4
     SAD_X%1_ONE 2*FENC_STRIDE+x*mmsize, 2*%2+x*mmsize
     %assign x x+1
 %endrep
@@ -404,7 +421,7 @@ cglobal pixel_vsad, 3,3,8
     jg .loop
 .end:
 %if BIT_DEPTH == 9
-    HADDW     m0, m1 ; max sum: 62(pixel diffs)*511(pixel_max)=31682
+    HADDW     m0, m1 ; max sum: 62(pixel diffs)*511(pixel_max)=31682 ; PIC*
 %else
     HADDUW    m0, m1 ; max sum: 62(pixel diffs)*1023(pixel_max)=63426
 %endif
@@ -442,7 +459,7 @@ cglobal pixel_vsad, 3,3
     jg .loop
 .end:
 %if BIT_DEPTH == 9
-    HADDW     m0, m1
+    HADDW     m0, m1 ; PIC*
 %else
     HADDUW    m0, m1
 %endif
@@ -458,18 +475,18 @@ cglobal pixel_sad_x%1_%2x%3, 6,7,XMM_REGS
     %assign regnum %1+1
     %xdefine STRIDE r %+ regnum
     mov     r6, %3/2-1
-    SAD_X%1_ONE_START
-    SAD_X%1_ONE 2*FENC_STRIDE, 2*STRIDE
-    SAD_X_2xNP %1, STRIDE, 1, %2/(mmsize/2)-1
+    SAD_X%1_ONE_START ; r0..r4*
+    SAD_X%1_ONE 2*FENC_STRIDE, 2*STRIDE ; r0..r4*
+    SAD_X_2xNP %1, STRIDE, 1, %2/(mmsize/2)-1 ; r0..r4*
 .loop:
-    SAD_X%1_INC_P
+    SAD_X%1_INC_P ; r0..r5*
     SAD_X_2xNP %1, STRIDE, 0, %2/(mmsize/2)
     dec     r6
     jg .loop
 %if %1 == 4
     mov     r6, r6m
 %endif
-    SAD_X%1_END %2, %3
+    SAD_X%1_END %2, %3 ; r0*, r5*, r6*, r5mp*, r6mp*, PIC*, RET
 %endmacro
 
 INIT_MMX mmx2
@@ -574,7 +591,8 @@ cglobal intra_sad_x3_4x4, 3,3,7
     pshuflw   m3, m3, q1111 ; FF FF EE EE
     pshuflw   m4, m4, q1111 ; HH HH GG GG
     paddw     m5, m3, m4
-    paddw     m6, [pw_4]
+    PIC_BEGIN r3
+    paddw     m6, [pic(pw_4)]
     paddw     m6, m5
     pshufd    m5, m5, q1032
     paddw     m5, m6
@@ -592,16 +610,17 @@ cglobal intra_sad_x3_4x4, 3,3,7
     movhlps   m3, m5
     paddw     m5, m3
     phaddw    m0, m5
-    pmaddwd   m0, [pw_1]
+    pmaddwd   m0, [pic(pw_1)]
     mova    [r2], m0
 %else
-    HADDW     m0, m3
+    HADDW     m0, m3 ; PIC*
     HADDW     m1, m3
     HADDW     m5, m3
     movd    [r2], m0 ; V prediction cost
     movd  [r2+4], m1 ; H prediction cost
     movd  [r2+8], m5 ; DC prediction cost
 %endif
+    PIC_END
     RET
 %endmacro
 
@@ -625,7 +644,7 @@ INTRA_SAD_X3_4x4
 ;m5 = temp
 ;m4 = pixel row
 
-%macro INTRA_SAD_HVDC_ITER 2
+%macro INTRA_SAD_HVDC_ITER 2 ; r0
     mova        m4, [r0+(%1-4)*FENC_STRIDEB]
     psubw       m4, m0
     ABSW        m4, m4, m5
@@ -648,11 +667,12 @@ cglobal intra_sad_x3_8x8, 3,3,8
     mova        m7, m0
     paddw       m0, m6
     punpckhwd   m7, m7
-    HADDW       m0, m4
-    paddw       m0, [pw_8]
+    PIC_BEGIN r3
+    HADDW       m0, m4 ; PIC*
+    paddw       m0, [pic(pw_8)]
     psrlw       m0, 4
     SPLATW      m0, m0
-    INTRA_SAD_HVDC_ITER 0, q3333
+    INTRA_SAD_HVDC_ITER 0, q3333 ; r0
     INTRA_SAD_HVDC_ITER 1, q2222
     INTRA_SAD_HVDC_ITER 2, q1111
     INTRA_SAD_HVDC_ITER 3, q0000
@@ -667,16 +687,17 @@ cglobal intra_sad_x3_8x8, 3,3,8
     movhlps     m3, m1
     paddw       m1, m3     ; 1 1 1 1 _ _ _ _
     phaddw      m2, m1     ; 2 2 3 3 1 1 _ _
-    pmaddwd     m2, [pw_1] ; 2 3 1 _
+    pmaddwd     m2, [pic(pw_1)] ; 2 3 1 _
     mova      [r2], m2
 %else
-    HADDW       m2, m4
+    HADDW       m2, m4 ; PIC*
     HADDW       m3, m4
     HADDW       m1, m4
     movd    [r2+0], m2
     movd    [r2+4], m3
     movd    [r2+8], m1
 %endif
+    PIC_END
     RET
 %endmacro
 
@@ -685,7 +706,7 @@ INTRA_SAD_X3_8x8
 INIT_XMM ssse3
 INTRA_SAD_X3_8x8
 
-%macro INTRA_SAD_HVDC_ITER_YMM 2
+%macro INTRA_SAD_HVDC_ITER_YMM 2 ; r0
     mova       xm4, [r0+(%1-4)*FENC_STRIDEB]
     vinserti128 m4, m4, [r0+%1*FENC_STRIDEB], 1
     pshufd      m5, m7, %2
@@ -707,12 +728,13 @@ cglobal intra_sad_x3_8x8, 3,3,8
     vbroadcasti128 m6, [r1+16*SIZEOF_PIXEL] ; V prediction
     vpermq         m7, m0, q0011
     paddw         xm0, xm6
-    paddw         xm0, [pw_1] ; equal to +8 after HADDW
-    HADDW         xm0, xm4
+    PIC_BEGIN r3
+    paddw         xm0, [pic(pw_1)] ; equal to +8 after HADDW
+    HADDW         xm0, xm4 ; PIC*
     psrld         xm0, 4
     vpbroadcastw   m0, xm0
     punpcklwd      m7, m7
-    INTRA_SAD_HVDC_ITER_YMM 0, q3333
+    INTRA_SAD_HVDC_ITER_YMM 0, q3333 ; r0
     INTRA_SAD_HVDC_ITER_YMM 1, q2222
     INTRA_SAD_HVDC_ITER_YMM 2, q1111
     INTRA_SAD_HVDC_ITER_YMM 3, q0000
@@ -722,6 +744,7 @@ cglobal intra_sad_x3_8x8, 3,3,8
     phaddw         m1, m3     ; 1 1 2 2 3 3 _ _ 1 1 2 2 3 3 _ _
     vextracti128  xm2, m1, 1
     paddw         xm1, xm2    ; 1 1 2 2 3 3 _ _
-    pmaddwd       xm1, [pw_1] ; 1 2 3 _
+    pmaddwd       xm1, [pic(pw_1)] ; 1 2 3 _
+    PIC_END
     mova         [r2], xm1
     RET
