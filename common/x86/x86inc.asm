b7da4b13b499a8d277d84fe386e92e7cee19c60c
Alexander Gattin <xrgtn@yandex.ru>
2024-07-31 23:02:07 +0000

    fix: declare IDs for R8b..R15b too

--- common/x86/x86inc.asm
+++ common/x86/x86inc.asm
@@ -941,7 +941,7 @@ rpicl:          pop rpic
 DECLARE_REG_ID rax, rcx, rdx, rbx, rsp, rbp, rsi, rdi, R8, R9, R10, R11, R12, R13, R14, R15
 DECLARE_REG_ID eax, ecx, edx, ebx, esp, ebp, esi, edi, R8d, R9d, R10d, R11d, R12d, R13d, R14d, R15d
 DECLARE_REG_ID ax, cx, dx, bx, sp, bp, si, di, R8w, R9w, R10w, R11w, R12w, R13w, R14w, R15w
-DECLARE_REG_ID al, cl, dl, bl, spl, bpl, sil, dil, R8w, R9w, R10w, R11w, R12w, R13w, R14w, R15w
+DECLARE_REG_ID al, cl, dl, bl, spl, bpl, sil, dil, R8b, R9b, R10b, R11b, R12b, R13b, R14b, R15b
 DECLARE_REG_ID ah, ch, dh, bh
 
 %macro CHECK_REG_COLLISION_I 3-4 ; reg, arg[i], i, arg[i].word
